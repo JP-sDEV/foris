72ffb0264b11bc85c06a9fed124eb6b2209ad082
JP-sDEV <jon.pablo80@gmail.com>
2025-07-22 14:34:35 -0400

    add primsa and user service

--- api/src/auth/auth.module.ts
+++ api/src/auth/auth.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthResolver } from './auth.resolver';
+import { PrismaService } from '../prisma/prisma.service';
+import { UserService } from '../user/user.service';
 
 @Module({
-  providers: [AuthResolver, AuthService],
+  providers: [AuthResolver, AuthService, PrismaService, UserService],
 })
 export class AuthModule {}
