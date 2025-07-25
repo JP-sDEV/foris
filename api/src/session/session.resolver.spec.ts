8f775d9ca0089fbf8d57c7011be83524c119f24d
JP-sDEV <jon.pablo80@gmail.com>
2025-07-25 08:35:13 -0400

    update resolver test

--- api/src/session/session.resolver.spec.ts
+++ api/src/session/session.resolver.spec.ts
@@ -1,13 +1,34 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { SessionResolver } from './session.resolver';
 import { SessionService } from './session.service';
+import { PrismaService } from '../prisma/prisma.service';
+import { UserService } from '../user/user.service';
 
 describe('SessionResolver', () => {
   let resolver: SessionResolver;
+  let prismaMock: any;
+  let userServiceMock: any;
 
   beforeEach(async () => {
+    prismaMock = {
+      session: {
+        create: jest.fn(),
+        findUnique: jest.fn(),
+        delete: jest.fn(),
+      },
+    };
+
+    userServiceMock = {
+      findOneByEmail: jest.fn(),
+    };
+
     const module: TestingModule = await Test.createTestingModule({
-      providers: [SessionResolver, SessionService],
+      providers: [
+        SessionResolver,
+        SessionService,
+        { provide: PrismaService, useValue: prismaMock },
+        { provide: UserService, useValue: userServiceMock },
+      ],
     }).compile();
 
     resolver = module.get<SessionResolver>(SessionResolver);
