98a16025bbf9f5c344d2b75f4a2faa19e89d8af0
JP-sDEV <jon.pablo80@gmail.com>
2025-08-05 14:32:13 -0400

    add forward refs

--- api/src/like/like.module.ts
+++ api/src/like/like.module.ts
@@ -1,11 +1,18 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { LikeService } from './like.service';
 import { LikeResolver } from './like.resolver';
-import { PrismaService } from 'src/prisma/prisma.service';
-import { AuthService } from 'src/auth/auth.service';
+import { PrismaService } from '../prisma/prisma.service';
+import { PostModule } from '../post/post.module';
+import { AuthModule } from '../auth/auth.module';
+import { UserModule } from '../user/user.module';
 
 @Module({
-  providers: [LikeResolver, LikeService, PrismaService, AuthService],
+  imports: [
+    PostModule,
+    forwardRef(() => AuthModule),
+    forwardRef(() => UserModule),
+  ],
+  providers: [LikeResolver, LikeService, PrismaService],
   exports: [LikeService],
 })
 export class LikeModule {}
