12543f8e5f42948732a65d5361037a1c5a70a0f4
JP-sDEV <jon.pablo80@gmail.com>
2025-08-14 15:01:17 -0400

    add challengestatus enum types

--- api/src/userchallenge/dto/update-userchallenge.input.ts
+++ api/src/userchallenge/dto/update-userchallenge.input.ts
@@ -1,6 +1,7 @@
 import { JoinUserChallengeInput } from './join-userchallenge.input';
 import { InputType, Field, PartialType } from '@nestjs/graphql';
-import { ChallengeStatus } from '@prisma/client';
+// import { ChallengeStatus } from '@prisma/client';
+import { ChallengeStatus } from '../enums/challenge-status';
 
 @InputType()
 export class UpdateUserChallengeInput extends PartialType(
