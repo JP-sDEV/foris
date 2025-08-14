c473674b3f0a5aeb6f780ae392a823f65f643bc1
JP-sDEV <jon.pablo80@gmail.com>
2025-08-14 15:02:48 -0400

    add integration userchallenge integration test

---
+++ api/test/int/userchallenge.int-spec.ts
@@ -0,0 +1,264 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import * as request from 'supertest';
+import { AppModule } from '../../src/app.module';
+import { PrismaService } from '../../src/prisma/prisma.service';
+import { v4 as uuidv4 } from 'uuid';
+import { ChallengeStatus } from '@prisma/client';
+import { JwtService } from '@nestjs/jwt';
+
+describe('UserchallengeModule (integration)', () => {
+  let app: INestApplication;
+  let prisma: PrismaService;
+  let token: string;
+  let jwtService: JwtService;
+  let userId: string;
+  let challengeId: string;
+
+  beforeAll(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+
+    await app.init();
+
+    prisma = app.get(PrismaService);
+    jwtService = app.get(JwtService);
+
+    // Clean DB
+    await prisma.userChallenge.deleteMany();
+    await prisma.challenge.deleteMany();
+    await prisma.user.deleteMany();
+
+    // Create user and challenge
+    const user = await prisma.user.create({
+      data: { id: uuidv4(), email: 'test@example.com', name: 'Test User' },
+    });
+    userId = user.id;
+
+    const challenge = await prisma.challenge.create({
+      data: {
+        id: uuidv4(),
+        name: 'Test Challenge',
+        createdBy: userId,
+      },
+    });
+    challengeId = challenge.id;
+
+    token = jwtService.sign(
+      { sub: userId },
+      { secret: process.env.JWT_SECRET },
+    );
+  });
+
+  afterAll(async () => {
+    await prisma.userChallenge.deleteMany();
+    await prisma.challenge.deleteMany();
+    await prisma.user.deleteMany();
+    await app.close();
+  });
+
+  it('should join a user challenge', async () => {
+    const mutation = `
+      mutation JoinUserChallenge($input: JoinUserChallengeInput!) {
+        joinUserChallenge(joinUserChallengeInput: $input) {
+          userId
+          challengeId
+          status
+        }
+      }
+    `;
+
+    const variables = { input: { challengeId } };
+
+    const response = await request(app.getHttpServer())
+      .post('/graphql')
+      .set('Authorization', `Bearer ${token}`)
+      .send({ query: mutation, variables });
+
+    // console.log(JSON.stringify(response.body.errors, null, 2));
+
+    expect(response.status).toBe(200);
+    expect(response.body.data.joinUserChallenge).toMatchObject({
+      userId,
+      challengeId,
+      status: ChallengeStatus.IN_PROGRESS,
+    });
+  });
+
+  it('should fetch the user challenge', async () => {
+    const query = `
+        query FindUserChallenge($input: JoinUserChallengeInput!) {
+          userchallenge(joinUserChallengeInput: $input) {
+            userId
+            challengeId
+            status
+          }
+        }
+      `;
+
+    const variables = { input: { challengeId } };
+
+    const response = await request(app.getHttpServer())
+      .post('/graphql')
+      .set('Authorization', `Bearer ${token}`)
+      .send({ query, variables });
+
+    expect(response.status).toBe(200);
+    expect(response.body.data.userchallenge).toMatchObject({
+      userId,
+      challengeId,
+      status: ChallengeStatus.IN_PROGRESS,
+    });
+  });
+
+  it('should update the user challenge', async () => {
+    const mutation = `
+        mutation UpdateUserChallenge($input: UpdateUserChallengeInput!) {
+          updateUserChallenge(updateUserChallengeInput: $input) {
+            userId
+            challengeId
+            status
+            completedAt
+          }
+        }
+      `;
+    const completedAt = new Date().toISOString();
+    const variables = {
+      input: { challengeId, status: ChallengeStatus.COMPLETED, completedAt },
+    };
+
+    const response = await request(app.getHttpServer())
+      .post('/graphql')
+      .set('Authorization', `Bearer ${token}`)
+      .send({ query: mutation, variables });
+
+    expect(response.status).toBe(200);
+    expect(response.body.data.updateUserChallenge).toMatchObject({
+      userId,
+      challengeId,
+      status: ChallengeStatus.COMPLETED,
+    });
+    expect(
+      new Date(
+        response.body.data.updateUserChallenge.completedAt,
+      ).toISOString(),
+    ).toBe(completedAt);
+  });
+
+  it('should remove the user challenge', async () => {
+    const mutation = `
+        mutation RemoveUserChallenge($input: JoinUserChallengeInput!) {
+          removeUserChallenge(joinUserChallengeInput: $input) {
+            userId
+            challengeId
+          }
+        }
+      `;
+    const variables = { input: { challengeId } };
+
+    const response = await request(app.getHttpServer())
+      .post('/graphql')
+      .set('Authorization', `Bearer ${token}`)
+      .send({ query: mutation, variables });
+
+    expect(response.status).toBe(200);
+    expect(response.body.data.removeUserChallenge).toMatchObject({
+      userId,
+      challengeId,
+    });
+  });
+
+  describe('Unauthorized requests', () => {
+    it('should not allow joining a user challenge without auth', async () => {
+      const mutation = `
+        mutation JoinUserChallenge($input: JoinUserChallengeInput!) {
+          joinUserChallenge(joinUserChallengeInput: $input) {
+            userId
+            challengeId
+            status
+          }
+        }
+      `;
+      const variables = { input: { challengeId } };
+
+      const response = await request(app.getHttpServer())
+        .post('/graphql')
+        .send({ query: mutation, variables });
+
+      expect(response.status).toBe(200); // GraphQL always returns 200
+      expect(response.body.errors).toBeDefined();
+      expect(response.body.errors[0].message).toBe('No authorization header');
+      expect(response.body.data).toBeNull();
+    });
+
+    it('should not allow fetching a user challenge without auth', async () => {
+      const query = `
+        query FindUserChallenge($input: JoinUserChallengeInput!) {
+          userchallenge(joinUserChallengeInput: $input) {
+            userId
+            challengeId
+            status
+          }
+        }
+      `;
+      const variables = { input: { challengeId } };
+
+      const response = await request(app.getHttpServer())
+        .post('/graphql')
+        .send({ query, variables });
+
+      expect(response.status).toBe(200);
+      expect(response.body.errors).toBeDefined();
+      expect(response.body.errors[0].message).toBe('No authorization header');
+      expect(response.body.data).toBeNull();
+    });
+
+    it('should not allow updating a user challenge without auth', async () => {
+      const mutation = `
+        mutation UpdateUserChallenge($input: UpdateUserChallengeInput!) {
+          updateUserChallenge(updateUserChallengeInput: $input) {
+            userId
+            challengeId
+            status
+          }
+        }
+      `;
+      const variables = {
+        input: { challengeId, status: ChallengeStatus.COMPLETED },
+      };
+
+      const response = await request(app.getHttpServer())
+        .post('/graphql')
+        .send({ query: mutation, variables });
+
+      expect(response.status).toBe(200);
+      expect(response.body.errors).toBeDefined();
+      expect(response.body.errors[0].message).toBe('No authorization header');
+      expect(response.body.data).toBeNull();
+    });
+
+    it('should not allow removing a user challenge without auth', async () => {
+      const mutation = `
+        mutation RemoveUserChallenge($input: JoinUserChallengeInput!) {
+          removeUserChallenge(joinUserChallengeInput: $input) {
+            userId
+            challengeId
+          }
+        }
+      `;
+      const variables = { input: { challengeId } };
+
+      const response = await request(app.getHttpServer())
+        .post('/graphql')
+        .send({ query: mutation, variables });
+
+      expect(response.status).toBe(200);
+      expect(response.body.errors).toBeDefined();
+      expect(response.body.errors[0].message).toBe('No authorization header');
+      expect(response.body.data).toBeNull();
+    });
+  });
+});
