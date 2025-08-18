fe69bf8ef83c9da710a4d55a7793d46f535965b8
JP-sDEV <jon.pablo80@gmail.com>
2025-08-18 12:15:58 -0400

    import challenge module

--- api/src/league/league.module.ts
+++ api/src/league/league.module.ts
@@ -3,9 +3,14 @@ import { LeagueService } from './league.service';
 import { LeagueResolver } from './league.resolver';
 import { UserModule } from '../user/user.module';
 import { AuthModule } from '../auth/auth.module';
+import { ChallengeModule } from '../challenge/challenge.module';
 
 @Module({
-  imports: [forwardRef(() => UserModule), forwardRef(() => AuthModule)],
+  imports: [
+    forwardRef(() => UserModule),
+    forwardRef(() => AuthModule),
+    forwardRef(() => ChallengeModule),
+  ],
   providers: [LeagueResolver, LeagueService],
   exports: [LeagueService],
 })
