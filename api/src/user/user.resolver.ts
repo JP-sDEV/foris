44f6b5a6d5460e8a2878d1ded85632c3acfaaed0
JP-sDEV <jon.pablo80@gmail.com>
2025-08-21 11:10:14 -0400

    add guard dec

--- api/src/user/user.resolver.ts
+++ api/src/user/user.resolver.ts
@@ -3,28 +3,42 @@ import { UserService } from './user.service';
 import { User } from './entities/user.entity';
 import { CreateUserInput } from './dto/create-user.input';
 import { UpdateUserInput } from './dto/update-user.input';
+import { UseGuards } from '@nestjs/common';
+import { GqlAuthGuard } from '../auth/auth.guard';
+import { CurrentUser } from '../common/decorators/current-user.decorator';
 
 @Resolver(() => User)
+@UseGuards(GqlAuthGuard)
 export class UserResolver {
   constructor(private readonly userService: UserService) {}
 
   @Mutation(() => User)
+  // @UseGuards(GqlAuthGuard)
   createUser(@Args('createUserInput') createUserInput: CreateUserInput) {
     return this.userService.create(createUserInput);
   }
 
   @Query(() => User, { name: 'user' })
+  @UseGuards(GqlAuthGuard)
   findOne(@Args('id', { type: () => String }) id: string) {
     return this.userService.findOneById(id);
   }
 
   @Mutation(() => User)
+  @UseGuards(GqlAuthGuard)
   updateUser(@Args('updateUserInput') updateUserInput: UpdateUserInput) {
     return this.userService.update(updateUserInput.id, updateUserInput);
   }
 
   @Mutation(() => User)
+  @UseGuards(GqlAuthGuard)
   removeUser(@Args('id', { type: () => String }) id: string) {
     return this.userService.remove(id);
+  }
+
+  @UseGuards(GqlAuthGuard)
+  @Query(() => User)
+  async me(@CurrentUser() user: User) {
+    return user;
   }
 }
